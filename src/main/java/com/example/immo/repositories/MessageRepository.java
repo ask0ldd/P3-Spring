6dda8d1f5e5eb8d87e5ec9407c21c597d9819488
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-22 17:01:01 +0100

    message repo fix

--- src/main/java/com/example/immo/repositories/MessageRepository.java
+++ src/main/java/com/example/immo/repositories/MessageRepository.java
@@ -3,6 +3,8 @@ package com.example.immo.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.immo.models.Message;
+
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
 
