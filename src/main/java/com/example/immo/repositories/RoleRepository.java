675cae418b74ce48fd4a096ff01de5cf592dd1fb
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-22 17:16:36 +0100

    role repo fix

---
+++ src/main/java/com/example/immo/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.immo.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.immo.models.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Optional<Role> findByAuthority(String authority);
+}
