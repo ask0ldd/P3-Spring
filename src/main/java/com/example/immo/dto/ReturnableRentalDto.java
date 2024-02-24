9da26517b6a9d96ea4129a8431bfa3e2c91d86d6
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-24 14:50:15 +0100

    returnrental dto

---
+++ src/main/java/com/example/immo/dto/ReturnableRentalDto.java
@@ -0,0 +1,26 @@
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class ReturnableRentalDto {
+    private Long rentalId;
+    private String description;
+    private String name;
+    private ReturnableUserDto owner;
+    private String picture;
+    private Float surface;
+    private Float price;
+
+    public ReturnableRentalDto(Rental rental) {
+        this.rentalId = rental.getRentalId();
+        this.description = rental.getDescription();
+        this.name = rental.getName();
+        this.picture = rental.getPicture();
+        this.surface = rental.getSurface();
+        this.price = rental.getPrice();
+        this.owner = new ReturnableUserDto(rental.getOwner());
+    }
+}
