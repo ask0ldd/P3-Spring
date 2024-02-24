1e52cd9878d3a5dc3d2bd1b193d538c63cb88ce1
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-24 14:49:10 +0100

    returnmessage dto

---
+++ src/main/java/com/example/immo/dto/ReturnableMessageDto.java
@@ -0,0 +1,21 @@
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class ReturnableMessageDto {
+    private Long messageId;
+    private ReturnableRentalDto rental;
+    private ReturnableUserDto user;
+    private String message;
+
+    public ReturnableMessageDto(Message message) {
+        super();
+        this.messageId = message.getMessageId();
+        this.message = message.getMessage();
+        this.user = new ReturnableUserDto(message.getUser());
+        this.rental = new ReturnableRentalDto(message.getRental());
+    }
+}
