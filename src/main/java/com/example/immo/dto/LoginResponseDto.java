d721800fe7555bf8866c1350f8b753b352ebe28e
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-24 14:45:34 +0100

    loginresp dto

---
+++ src/main/java/com/example/immo/dto/LoginResponseDto.java
@@ -0,0 +1,31 @@
+public class LoginResponseDto {
+    private LoggedUserDto user;
+    private String jwt;
+
+    public LoginResponseDto() {
+        super();
+    }
+
+    public LoginResponseDto(User user, String jwt) {
+        super();
+        this.user = new LoggedUserDto(user); // return a simplified version of the user model
+        this.jwt = jwt;
+    }
+
+    public LoggedUserDto getUser() {
+        return this.user;
+    }
+
+    public String getJwt() {
+        return this.jwt;
+    }
+
+    public void setUser(LoggedUserDto user) {
+        this.user = user;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+}
