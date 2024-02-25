e822454c27db11a1ff72a0b40fd14735ccda192c
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-25 20:07:15 +0100

    all imports but services fixed

--- src/main/java/com/example/immo/controllers/AuthController.java
+++ src/main/java/com/example/immo/controllers/AuthController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.immo.dto.LoginResponseDto;
+import com.example.immo.dto.RegistrationDto;
 import com.example.immo.models.User;
 
 @RestController
--- src/main/java/com/example/immo/controllers/MessageController.java
+++ src/main/java/com/example/immo/controllers/MessageController.java
@@ -1,7 +1,9 @@
 package com.example.immo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.immo.dto.ReturnableMessageDto;
+import com.example.immo.models.Message;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
