5d9b1c9c70b7986d9980f0dd0c6bcaea4310b353
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-27 11:32:17 +0100

    returnable rental dto matching front expect

--- src/main/java/com/example/immo/dto/ReturnableRentalDto.java
+++ src/main/java/com/example/immo/dto/ReturnableRentalDto.java
@@ -1,5 +1,7 @@
 package com.example.immo.dto;
 
+import java.util.Date;
+
 import com.example.immo.models.Rental;
 
 import lombok.Data;
@@ -10,21 +12,26 @@ import lombok.Setter;
 @Setter
 @Getter
 public class ReturnableRentalDto {
-    private Long rentalId;
+    private Long id;
     private String description;
     private String name;
-    private ReturnableUserDto owner;
+    // private ReturnableUserDto owner;
+    private Long owner_id;
     private String picture;
-    private Float surface;
-    private Float price;
+    private Integer surface;
+    private Integer price;
+    private Date created_at;
+    private Date updated_at;
 
     public ReturnableRentalDto(Rental rental) {
-        this.rentalId = rental.getRentalId();
+        this.id = rental.getRentalId();
         this.description = rental.getDescription();
         this.name = rental.getName();
         this.picture = rental.getPicture();
         this.surface = rental.getSurface();
         this.price = rental.getPrice();
-        this.owner = new ReturnableUserDto(rental.getOwner());
+        this.owner_id = rental.getOwner().getUserId();
+        this.created_at = rental.getCreation();
+        this.updated_at = rental.getUpdate();
     }
 }
