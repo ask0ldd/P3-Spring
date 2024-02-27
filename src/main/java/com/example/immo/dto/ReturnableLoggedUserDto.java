0b47a49bbc64c35a1fee32b2a21535271d5b799d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-27 11:30:00 +0100

    returnable logged user dto

---
+++ src/main/java/com/example/immo/dto/ReturnableLoggedUserDto.java
@@ -0,0 +1,26 @@
+package com.example.immo.dto;
+
+import java.util.Date;
+
+import com.example.immo.models.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReturnableLoggedUserDto { // return a simplified version of the user model
+    private Long id;
+    private String name;
+    private String email;
+    private Date created_at;
+    private Date updated_at;
+
+    public ReturnableLoggedUserDto(User user) {
+        this.id = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.created_at = user.getCreation();
+        this.updated_at = user.getUpdate();
+    }
+}
