0640ce5958e87f7c73d37394e852b7368ed99db5
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-28 02:02:17 +0100

    security config clean up

--- src/main/java/com/example/immo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/immo/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+// import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -51,54 +53,43 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authManager(UserDetailsService userDetailsService) {
+    AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
         daoProvider.setUserDetailsService(userDetailsService);
         daoProvider.setPasswordEncoder(passwordEncoder());
         return new ProviderManager(daoProvider);
     }
 
+    // !!!!!!!!!!!!!! protect routes
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
                 .cors(Customizer.withDefaults())
-                // .headers(header -> header.contentTypeOptions(null))
-                // .headers(header -> header.contentTypeOptions())
                 .authorizeHttpRequests(authorize -> {
                     authorize
-                            // .dispatcherTypeMatchers(DispatcherType.ERROR,
-                            // DispatcherType.FORWARD).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/test1")).hasRole("ADMIN")
-                            .requestMatchers(new AntPathRequestMatcher("/test2")).hasRole("USER")
-                            .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/api/auth/login/**")).permitAll()
-                            // rentals working
+                            .requestMatchers(new AntPathRequestMatcher("/api/auth/login")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/api/auth/register")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/api/auth/me")).permitAll()
                             .requestMatchers(new AntPathRequestMatcher("/api/rentals/**")).permitAll()
                             .requestMatchers(new AntPathRequestMatcher("/auth/*")).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/users2")).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/user/*")).permitAll()
-                            .requestMatchers(new AntPathRequestMatcher("/rental/*", "PUT")).hasRole("ADMIN")
                             .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(
                         oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> jwt.decoder(jwtDecoder())
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // allows h2-console frames to be displayed
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())/* .disable() */)
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .build();
-        // html login
-        // .formLogin(Customizer.withDefaults()).build();
     }
 
     @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
         jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
@@ -115,6 +106,9 @@ public class SecurityConfiguration {
 // https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html#authorize-requests
 // Basic Auth
 // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/basic.html
+
+// oauth server jwt
+// https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html
 
 /*
  * 
@@ -148,3 +142,6 @@ public class SecurityConfiguration {
  */
 // basic popup login form
 // .httpBasic(Customizer.withDefaults())
+
+// latest example
+// https://github.com/spring-projects/spring-security-samples/blob/6.2.x/servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
