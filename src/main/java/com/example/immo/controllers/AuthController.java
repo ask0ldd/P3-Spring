66bdb4934a39b8117bc5b6addaf60ad8e2eb9685
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-03-02 01:34:57 +0100

    auth controller exceptions

--- src/main/java/com/example/immo/controllers/AuthController.java
+++ src/main/java/com/example/immo/controllers/AuthController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.immo.dto.ReturnableLoggedUserDto;
 import com.example.immo.dto.LoginDto;
 import com.example.immo.dto.RegistrationDto;
 import com.example.immo.dto.TokenResponseDto;
+import com.example.immo.dto.responses.ResponseLoggedUserDto;
 import com.example.immo.models.User;
 import com.example.immo.services.AuthService;
 import com.example.immo.services.UserService;
@@ -24,7 +24,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("api/auth")
-// @CrossOrigin(origins = "http://localhost:5173")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
@@ -36,23 +35,57 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<TokenResponseDto> userRegister(@RequestBody RegistrationDto body) {
-        authService.registerUser(body.getEmail(), body.getUsername(), body.getPassword());
-        TokenResponseDto token = authService.loginUser(body.getEmail(), body.getPassword());
-        return new ResponseEntity<>(token, HttpStatus.OK);
+        try {
+            authService.registerUser(body.getEmail(), body.getUsername(), body.getPassword());
+            TokenResponseDto token = authService.loginUser(body.getEmail(), body.getPassword());
+            if (token == null || token.toString().isEmpty())
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> userLogin(@RequestBody LoginDto body) {
-        TokenResponseDto token = authService.loginUser(body.getEmail(), body.getPassword());
-        return new ResponseEntity<>(token, HttpStatus.OK);
+    public ResponseEntity<?> userLogin(@RequestBody LoginDto body) {
+        try {
+            TokenResponseDto token = authService.loginUser(body.getEmail(), body.getPassword());
+            if (token == null) {
+                return new ResponseEntity<String>("error", HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity<TokenResponseDto>(token, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+    /*
+     * @GetMapping("/me")
+     * public ResponseEntity<?> getLoggedUser(HttpServletRequest request) {
+     * Principal principal = request.getUserPrincipal();
+     * String email = principal.getName();
+     * User loggedUser = userService.getUserByEmail(email);
+     * return new ResponseEntity<ReturnableLoggedUserDto>(new
+     * ReturnableLoggedUserDto(loggedUser),
+     * HttpStatus.OK);
+     * }
+     */
 
     @GetMapping("/me")
     public ResponseEntity<?> getLoggedUser(HttpServletRequest request) {
-        Principal principal = request.getUserPrincipal();
-        String email = principal.getName();
-        User loggedUser = userService.getUserByEmail(email);
-        return new ResponseEntity<ReturnableLoggedUserDto>(new ReturnableLoggedUserDto(loggedUser),
-                HttpStatus.OK);
+        try {
+            Principal principal = request.getUserPrincipal();
+            if (principal == null) {
+                return new ResponseEntity<String>("User not authenticated", HttpStatus.UNAUTHORIZED);
+            }
+            String email = principal.getName();
+            User loggedUser = userService.getUserByEmail(email);
+            if (loggedUser == null) {
+                return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<ResponseLoggedUserDto>(new ResponseLoggedUserDto(loggedUser), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
